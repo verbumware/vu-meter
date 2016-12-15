6253806f88cd8989d5ad214e12ca70ffa7c719cb
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-12-15 00:59:45 -0800

    added WAV header placeholder into saved file

--- demo2/server.js
+++ demo2/server.js
@@ -7,8 +7,27 @@ var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function (ws) {
-    var fname = path.resolve(process.cwd(), 'F' + Date.now() + '.bin');
+    var fname = path.resolve(process.cwd(), 'F' + Date.now() + '.wav');
     console.log(fname);
+    fs.appendFileSync(
+        fname,
+        Buffer.from(
+            '52494646' + // ChunkID: "RIFF"
+            '24080000' + // ChunkSize: 2084???
+            '57415645' + // Format: "WAVE"
+            '666d7420' + // "fmt "
+            '10000000' + // Subchunk1Size = 16
+            '0100'     + // AudioFormat = PCM
+            '0100'     + // NumChannels = mono
+            '803e0000' + // SampleRate: 16000
+            '007d0100' + // ByteRate: 32000
+            '0200'     + // BlockAlign: 2
+            '1000'     + // BitsPerSample: 16
+            '64617461' + // "data"
+            '00080000', // Subchunk2Size: 2048
+            'hex'),
+        { encoding: 'binary' }
+    );
     ws.on('message', function (message) {
         fs.appendFileSync(fname, message, { encoding: 'binary' });
     });
